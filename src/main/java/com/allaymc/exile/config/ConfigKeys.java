c0980770fa9f3f5972b1f6e46b6bc33914dae357
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-30 00:00:57 +0530

    Create ConfigKeys.java

---
+++ src/main/java/com/allaymc/exile/config/ConfigKeys.java
@@ -0,0 +1,21 @@
+package com.allaymc.exile.config;
+
+public final class ConfigKeys {
+    private ConfigKeys() {}
+
+    public static final String OVERWORLD_WORLD = "borders.overworld.world";
+    public static final String OVERWORLD_CENTER_X = "borders.overworld.center-x";
+    public static final String OVERWORLD_CENTER_Z = "borders.overworld.center-z";
+    public static final String OVERWORLD_MAIN_SIZE = "borders.overworld.main-size-blocks";
+    public static final String OVERWORLD_EXILE_THRESHOLD = "borders.overworld.exile-threshold-blocks";
+    public static final String OVERWORLD_EXILE_SPAWN_MIN = "borders.overworld.exile-spawn-min";
+    public static final String OVERWORLD_EXILE_SPAWN_MAX = "borders.overworld.exile-spawn-max";
+
+    public static final String NETHER_WORLD = "borders.nether.world";
+    public static final String NETHER_CENTER_X = "borders.nether.center-x";
+    public static final String NETHER_CENTER_Z = "borders.nether.center-z";
+    public static final String NETHER_MAIN_SIZE = "borders.nether.main-size-blocks";
+    public static final String NETHER_EXILE_THRESHOLD = "borders.nether.exile-threshold-blocks";
+    public static final String NETHER_EXILE_SPAWN_MIN = "borders.nether.exile-spawn-min";
+    public static final String NETHER_EXILE_SPAWN_MAX = "borders.nether.exile-spawn-max";
+}
