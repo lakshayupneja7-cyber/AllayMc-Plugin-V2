6a1a788789faa9f6290bb4971556ef521a3f09e7
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-30 00:01:25 +0530

    Create Messages.java

---
+++ src/main/java/com/allaymc/exile/config/Messages.java
@@ -0,0 +1,14 @@
+package com.allaymc.exile.config;
+
+public final class Messages {
+    private Messages() {}
+
+    public static final String NO_PERMISSION = "no-permission";
+    public static final String PLAYER_ONLY = "player-only";
+    public static final String PLAYER_NOT_FOUND = "player-not-found";
+    public static final String INVALID_TIME = "invalid-time";
+    public static final String INVALID_NUMBER = "invalid-number";
+    public static final String NOT_EXILED = "not-exiled";
+    public static final String ALREADY_EXILED = "already-exiled";
+    public static final String CASE_NOT_FOUND = "case-not-found";
+}
